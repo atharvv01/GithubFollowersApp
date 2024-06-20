6477bca83a69db4fbddcc3ec46d25f73555f9cb1
atharvv01 <athzanwar@gmail.com>
2024-06-20 15:31:24 +0530

    Created tabbar controller

---
+++ GithubFollowersApp/FavoritesListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListViewController.swift
+//  GithubFollowersApp
+//
+//  Created by E5000866 on 19/06/24.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+   
+}
---
+++ GithubFollowersApp/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GithubFollowersApp
+//
+//  Created by E5000866 on 19/06/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+}
