2b73cd2558b203b6ed3639dc3bd2524908a4d2ac
atharvv01 <athzanwar@gmail.com>
2024-06-24 16:58:41 +0530

    Made models for user and followers

---
+++ GithubFollowersApp/Follower.swift
@@ -0,0 +1,12 @@
+import Foundation
+ 
+struct Follower : Codable {
+    //now here we need to match the name of entities to that in json response
+    var login : String
+    
+    /*
+     here although the name is 'avatar_url' since we use camel case in swift
+     codable has it taken care of that it understands theres a underscore
+     */
+    var avatarUrl:String
+}
